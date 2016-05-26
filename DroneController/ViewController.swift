2e947c0df38104b60a95b28d8d029e02e9dc2075
crjang <crjang91@gmail.com>
2016-05-26 12:42:50 +0900

    Initial Commit

---
+++ DroneController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DroneController
+//
+//  Created by JangChorong on 2016. 5. 26..
+//  Copyright © 2016년 Aerodyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
