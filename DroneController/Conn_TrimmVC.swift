169ba5deb6422e1b84997f43594c113df45d9910
crjang <crjang91@gmail.com>
2016-05-31 23:56:03 +0900

    button - round corner

--- DroneController/Conn_TrimmVC.swift
+++ DroneController/Conn_TrimmVC.swift
@@ -6,8 +6,9 @@ import UIKit
 
 class Conn_TrimmVC: UIViewController {
 
+
+    
     @IBAction func dismiss(sender: AnyObject) {
         self.presentingViewController?.dismissViewControllerAnimated(false, completion: nil)
     }
-    
 }
