f25ae1c2c0c32129874b6e500c210f0c85328f8e
crjang <crjang91@gmail.com>
2016-06-03 20:38:30 +0900

    init joystick

--- DroneController/ViewController.swift
+++ DroneController/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let scene = GameScene(size: self.view.bounds.size)
+        scene.backgroundColor = .whiteColor()
+        
+        if let skView = self.view as? SKView {
+            
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            /* Set the scale mode to scale to fit the window */
+            //scene.scaleMode = .AspectFill
+            skView.presentScene(scene)
+        }else {
+            print(2)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
 
