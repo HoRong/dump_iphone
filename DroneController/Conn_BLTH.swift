6f57b5080c1511a63f6d0bdda92cf396ebc26d0e
crjang <crjang91@gmail.com>
2016-06-18 17:16:50 +0900

    Bluetooth #3 - send data test

--- DroneController/Conn_BLTH.swift
+++ DroneController/Conn_BLTH.swift
@@ -3,18 +3,19 @@ import CoreBluetooth
 
 class Conn_BLTH: UIViewController,UITableViewDataSource, UITableViewDelegate, BluetoothSerialDelegate {
     
+    @IBOutlet weak var btn_bluetooth: UIButton!
     @IBOutlet weak var tableView: UITableView!
     var peripherals: [(peripheral: CBPeripheral, RSSI: Float)] = []
-    let textCellIdentifier = "cell"
+    
     var selectedPeripheral: CBPeripheral?
+    let textCellIdentifier = "cell"
     
     @IBOutlet weak var testLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
       
-      tableView.tableFooterView = UIView(frame: CGRectZero)
+        tableView.tableFooterView = UIView(frame: CGRectZero)
         
         serial = BluetoothSerial(delegate: self)
         serial.writeType = .WithoutResponse
@@ -22,20 +23,32 @@ class Conn_BLTH: UIViewController,UITableViewDataSource, UITableViewDelegate, Bl
     }
 
     
+    @IBAction func dismiss(sender: AnyObject) {
+        self.presentingViewController?.dismissViewControllerAnimated(false, completion: nil)
+    }
+    
+    
     func serialDidReceiveString(message: String) {}
     
     func serialDidDisconnect(peripheral: CBPeripheral, error: NSError?) {
         
     }
     
+    func serialIsReady(peripheral: CBPeripheral) {
+       print("test " + "connect device 11")
+      
+    }
+    
     func serialDidChangeState(newState: CBCentralManagerState) {
-        
+        print("test " + "connect device 22")
     }
 
     
     @IBAction func scanBluetooth(sender: AnyObject) {
         serial.startScan()
-        print(11);
+        
+        btn_bluetooth.enabled = false;
+        btn_bluetooth.backgroundColor = UIColor.grayColor()
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -48,27 +61,28 @@ class Conn_BLTH: UIViewController,UITableViewDataSource, UITableViewDelegate, Bl
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // return a cell with the peripheral name as text in the label
-        
-        print(22);
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier)!
-        cell.textLabel?.text = peripherals[indexPath.row].peripheral.name
+        
+        let label = cell.viewWithTag(1) as! UILabel!
+        label.text = peripherals[indexPath.row].peripheral.name
 
         return cell
     }
     
     
     //MARK: UITableViewDelegate
-    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
+        print("test " + "select device")
         // the user has selected a peripheral, so stop scanning and proceed to the next view
         serial.stopScan()
         selectedPeripheral = peripherals[indexPath.row].peripheral
         serial.connectToPeripheral(selectedPeripheral!)
+        
+        serial.sendMessageToDevice("send init data")
     }
-    
     
     func serialDidDiscoverPeripheral(peripheral: CBPeripheral, RSSI: NSNumber?) {
         // check whether it is a duplicate
